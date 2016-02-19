444aa0d6030be2442090d7e7e729eaa23a1aa910
Matthew Kindy II <mpk2@rice.edu>
2016-02-19 11:45:21 -0600

    Finishing up work on reformPiecewise... not done yet

--- reformPiecewise.m
+++ reformPiecewise.m
@@ -2,9 +2,53 @@
 %   piecewiseProblem has properties
 %
 %
-function convexProblem = reformPiecewise(piecewiseProblem)
+function convexProblem = reformPiecewise(initProb, piecewiseConstraints)
 
+% piecewise constraints are of form [x;y]
+k = convhull([0 piecewiseConstraints(1,:)], [0 piecewiseConstraints(2,:)]);
 
+% Go through all the segments
+for i=1:length(k)
+    
+    % Form the individual linear constraints
+    m = (y1-y2) / (x1-x2);
+    b = x1-m*y2;
+    
+    % Over each month
+    for j = 1:n
+        
+        % Preallocate an empty row for this constraint
+        inventoryVector = zeros(1,2*n);
 
+        % We want to limit the sth volume, which is equivalent to limiting
+        % contracts
+        inventoryVector(1:j-1) = -dpm(1:j-1);
+        inventoryVector(n+1:n+j-1) = dpm(1:j-1);
+
+        % Add in the current day worth of injection/withdrawal (first day of
+        % the month)
+        inventoryVector(k) = -1;
+        inventoryVector(n+j) = 1;
+
+        % Preallocate an empty row for this injection constraint
+        injectionVector = zeros(1,2*n);
+        withdrawalVector = zeros(1,2*n);
+
+        % Subtract the delivered contracts from the exercised contracts for
+        % this month
+        injectionVector(j) = -1;
+        injectionVector(n+j) = 1;
+
+        % Subtract the exercised contracts from the delivered contracts for
+        % this month
+        withdrawalVector(j) = 1;
+        withdrawalVector(n+j) = -1;
+        
+        newA = injectionVector-m*inventoryVector;
+    end
+
+    initProb.Aineq = [initProb.Aineq; newA];
+    initProb.b = [initProb.b b];
+end
 
 end
